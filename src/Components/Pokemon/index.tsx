88e97cd99c83499dac653e248e61675593b84a51
Pierre Souza <pas@lionx.com.br>
2022-07-15 11:31:13 -0300

    chore: remove mock component

--- src/Components/Pokemon/index.tsx
+++ src/Components/Pokemon/index.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../assets/pokemon-logo.png";
+import pokeball from "../../assets/pokeball.png";
+import { usePokemonsQuery } from "../../generated/graphql";
+import { Container, Content, Item, InputSearch } from "./styles";
 import { MockedPokemon } from "./mock";
-import { Container, InputSearch } from "./styles";
 
 export const Pokemon = () => {
+  const { data } = usePokemonsQuery();
+
   return (
     <Container>
       <InputSearch>
@@ -12,7 +16,15 @@ export const Pokemon = () => {
           <input type="text" placeholder="Search Pokemon" />
         </div>
       </InputSearch>
-      <MockedPokemon />
+
+      <Content>
+        {data?.gen3_species.map(({ id, name }) => (
+          <Item key={id}>
+            {name}
+            <img src={pokeball} alt="" />
+          </Item>
+        ))}
+      </Content>
     </Container>
   );
 };
